b62cf977c482435c13a195a16274e2e71e437434
Abe Sanchez <abraham5001@aol.com>
2017-03-13 04:53:17 -0400

    AI is invincible

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 var tiles = document.getElementsByClassName("tile");
 var buttons = document.getElementsByClassName('buttons');
 var winnerExists = false;
-var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var state = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var winMatrix = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]];
 
 function claim(tile) {
-    if (isntClaimed(tile) && winnerExists == false) {
+    if (isntClaimed(tile) && !winnerExists) {
         tile.innerHTML = 'X';
-        board[tile.id] = 1;
-        checkForWinner();
-        if (winnerExists == false) {
-            aiTurn();
+        state[tile.id] = 1;
+        checkForWinner(state);
+        if (!winnerExists) {
+            aiTurn(state, 0, true);
         }
     }
-    checkForWinner();
+    checkForWinner(state);
+}
+
+function checkWin(board, player){
+    var value = player != true ? 1 : 2;
+
+    for(var i = 0; i < 8; i++){
+      var win = true;
+      for(var j = 0; j < 3; j++){
+        if(board[winMatrix[i][j]] != value){
+          win = false;
+        }
+      }
+      if(win){
+        return true;
+      }
+    }
+    return false;
 }
 
 function isntClaimed(tile) {
@@ -23,34 +49,37 @@ function isntClaimed(tile) {
     }
 }
 
-function checkForWinner() {
-    if (tiles[0].innerHTML == tiles[1].innerHTML && tiles[1].innerHTML == tiles[2].innerHTML && !(isntClaimed(tiles[2]))) {
+function checkForWinner(board) {
+    if (board[0] == board[1] && board[1] == board[2] && !(isntClaimed(tiles[2]))) {
         win(tiles[0], tiles[1], tiles[2]);
-    } else if (tiles[3].innerHTML == tiles[4].innerHTML && tiles[4].innerHTML == tiles[5].innerHTML && !(isntClaimed(tiles[5]))) {
+    } else if (board[3] == board[4] && board[4] == board[5] && !(isntClaimed(tiles[5]))) {
         win(tiles[3], tiles[4], tiles[5]);
-    } else if (tiles[6].innerHTML == tiles[7].innerHTML && tiles[7].innerHTML == tiles[8].innerHTML && !(isntClaimed(tiles[8]))) {
+    } else if (board[6] == board[7] && board[7] == board[8] && !(isntClaimed(tiles[8]))) {
         win(tiles[6], tiles[7], tiles[8]);
-    } else if (tiles[0].innerHTML == tiles[3].innerHTML && tiles[3].innerHTML == tiles[6].innerHTML && !(isntClaimed(tiles[6]))) {
+    } else if (board[0] == board[3] && board[3] == board[6] && !(isntClaimed(tiles[6]))) {
         win(tiles[0], tiles[3], tiles[6]);
-    } else if (tiles[1].innerHTML == tiles[4].innerHTML && tiles[4].innerHTML == tiles[7].innerHTML && !(isntClaimed(tiles[7]))) {
+    } else if (board[1] == board[4] && board[4] == board[7] && !(isntClaimed(tiles[7]))) {
         win(tiles[1], tiles[4], tiles[7]);
-    } else if (tiles[2].innerHTML == tiles[5].innerHTML && tiles[5].innerHTML == tiles[8].innerHTML && !(isntClaimed(tiles[8]))) {
+    } else if (board[2] == board[5] && board[5] == board[8] && !(isntClaimed(tiles[8]))) {
         win(tiles[2], tiles[5], tiles[8]);
-    } else if (tiles[0].innerHTML == tiles[4].innerHTML && tiles[4].innerHTML == tiles[8].innerHTML && !(isntClaimed(tiles[8]))) {
+    } else if (board[0] == board[4] && board[4] == board[8] && !(isntClaimed(tiles[8]))) {
         win(tiles[0], tiles[4], tiles[8]);
-    } else if (tiles[6].innerHTML == tiles[4].innerHTML && tiles[4].innerHTML == tiles[2].innerHTML && !(isntClaimed(tiles[2]))) {
+    } else if (board[6] == board[4] && board[4] == board[2] && !(isntClaimed(tiles[2]))) {
         win(tiles[6], tiles[4], tiles[2]);
     }
 
-    var boardFull = true;
-    for (var i = 0; i < board.length; i++) {
-        if(board[i] == 0){
-          boardFull = false;
-        }
-    }
-    if (boardFull == true){
+    if (boardFull(board)){
       winnerExists = true;
     }
+}
+
+function boardFull(board){
+  for (var i = 0; i < board.length; i++) {
+      if(board[i] == 0){
+        return false;
+      }
+  }
+  return true;
 }
 
 function win(x, y, z) {
@@ -63,19 +92,45 @@ function win(x, y, z) {
 
 function reset() {
     for (var i = 0; i < tiles.length; i++) {
-        board[i] = 0;
+        state[i] = 0;
         tiles[i].innerHTML = "";
         tiles[i].style.background = "white"
     }
     winnerExists = false;
 }
 
-function aiTurn() {
-    var tileToClaim;
-    while (board[tileToClaim] != 0) {
-        tileToClaim = Math.floor(Math.random() * 10);
-        console.log(tileToClaim);
+function aiTurn(board, depth, ai) {
+    if(checkWin(board, !ai)){
+      return -10 + depth;
     }
-    tiles[tileToClaim].innerHTML = 'O';
-    board[tileToClaim] = 2;
+
+    if(boardFull(board)){
+      return 0;
+    }
+
+    var value = ai == true ? 2 : 1;
+
+    var max = -Infinity;
+    var index = 0;
+
+    for (var i = 0; i < board.length; i++){
+      if(board[i] == 0){
+        var newBoard = board.slice();
+        newBoard[i] = value;
+
+        var moveVal = -aiTurn(newBoard, depth + 1, !ai);
+
+        if(moveVal > max){
+          max = moveVal;
+          index = i;
+        }
+      }
+    }
+
+    if(depth == 0){
+      tiles[index].innerHTML = 'O';
+      state[index] = 2;
+    }
+
+    return max;
 }
